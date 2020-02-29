57f2969fc894dd354f08a5771744412f06bb61ba
bkumar2018 <36272148+bkumar2018@users.noreply.github.com>
2020-02-29 16:39:10 +0530

    created ReadDB.java

---
+++ ReadDB.java
@@ -0,0 +1,61 @@
+package com.DataBase;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.apache.commons.lang3.SystemUtils;
+import org.apache.poi.util.SystemOutLogger;
+
+public class ReadDBFile {
+
+	static Object[][] obj ;
+	
+	public static void main(String[] args) throws ClassNotFoundException, SQLException{
+		System.out.println("DB");
+		ReadDBFile rdb = new ReadDBFile();
+		obj = rdb.readDB();
+		for (int i = 0; i < obj.length; i++) {
+			for (int j = 0; j < obj[i].length; j++) {
+				System.out.print(obj[i][j] + " ");
+			}
+			System.out.println("");
+		}		
+		
+	}
+	
+	
+	public Object[][] readDB() throws ClassNotFoundException, SQLException{
+		
+		Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");  
+		String connectionUrl = "jdbc:sqlserver://DESKTOP-6A7BK1O;DatabaseName=loginDB;integratedSecurity=true";
+		//String connectionUrl = "jdbc:sqlserver://DESKTOP-6A7BK1O;database=loginDB;integratedSecurity=true";  
+		Connection con = DriverManager.getConnection(connectionUrl,"sa","root123");  
+		Statement stmt=con.createStatement(); 
+		ResultSet rs=stmt.executeQuery("select * from login"); 
+		
+		int i =0;
+		int j =0;
+		obj = new Object[3][2];
+		
+		while(rs.next()){  
+		//	System.out.println(rs.getInt(1)+" "+rs.getString(2)+" "+rs.getString(3)); 			
+			obj[i][j]= rs.getString(2);			
+		//	System.out.println("col1:"+obj[i][j]);
+			j++;
+			obj[i][j]=rs.getString(3);
+		//	System.out.println("col2:"+obj[i][j]);
+			i++;
+			j=0;
+		}  
+		con.close();
+
+		return obj;
+	}
+	
+	public void setupDB(){
+		
+	}
+}
